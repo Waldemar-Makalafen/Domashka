c4b8700e40e5fb73ac8ff6bf835e0be5825a290b
Waldemar-Makalafen <67731457+Waldemar-Makalafen@users.noreply.github.com>
2020-07-02 10:41:43 +0300

    Новые файлы для домашки
    Новые файлы для домашки

---
+++ Zadanie_1.py
@@ -0,0 +1,37 @@
+﻿name = str(input('Введите ваше имя'))
+age = int(input('Введите ваш возраст'))
+
+age = age - 18
+if age > 0:
+	if age <5:
+		print (name, 'на',age, 'год/года старше 18')
+	else:
+		print (name, 'на',age, 'лет старше 18')
+else:
+	age = age*(-1)
+	print (name, 'на',age, 'года/лет младше 18')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Chtoby_ne_vilitalo = input ()
