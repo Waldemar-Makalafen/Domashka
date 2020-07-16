8192d921382296d3c960e04dd44afc553d632c2d
Waldemar-Makalafen <67731457+Waldemar-Makalafen@users.noreply.github.com>
2020-07-16 14:58:31 +0300

    домашка для 5 задания

---
+++ Dl Academy/Лекция_первое_занятие/5занятие/easy/PN1.py
@@ -0,0 +1,4 @@
+﻿
+list1 = [x**2 for x in range(1,5)]
+
+print(list1)
---
+++ Dl Academy/Лекция_первое_занятие/5занятие/easy/PN3.py
@@ -0,0 +1,17 @@
+﻿lst = []
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+lst.append(a)
+lst.append(b)
+lst.append(c)
+lst.append(d)
+
+
+lst1 = [x for x in lst if x >= 0 and x%4 and not x%3]
+
+print(lst1)
+
+
